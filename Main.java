70f0db5df195d06785cfe25b4cc9070f6b813ec0
José Ovando <42974480+Jos260400@users.noreply.github.com>
2019-03-02 16:04:42 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+// Universidad del Valle de Guatemala
+// Curso: Algoritmos y Estructuras de Datos
+// Nombre: Fernando José Garavito Ovando	 Carné: 18071
+// Hoja de Trabajo No. 6
+
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
