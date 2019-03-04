1f57fb6f9207109922c94717027e08fa28e47599
José Ovando <42974480+Jos260400@users.noreply.github.com>
2019-03-03 23:08:46 -0600

    Add files via upload

--- Factory.java
+++ Factory.java
@@ -3,23 +3,19 @@ package Classes;
 // Curso: Algoritmos y Estructuras de Datos
 // Nombre: Fernando José Garavito Ovando	 Carné: 18071
 // Hoja de Trabajo No. 6
+
+// Importamos cada una de las líbrerias para nuestra interface
+import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.TreeMap;
 
-public class Factory<K,V>{
 
-    /*
-    HashMap hashmap = new HashMap();
-    TreeMap treemap= new TreeMap();
-    LinkedHashMap linkedhashmap= new LinkedHashMap();
-     */
+// Ponemos la llave y valor del Map que son en este caso las letras "F" y "G"
+// Elegí esas dos porque son las letras de mi primero nombre y primer apellido
+public class Factory<F,G>{
 
-    //This method has the function of gave one of the three mapping style.
-    //depending of the choice of the user.
-
-    public Map<K,V> SetType(String type){
+    public Map<F,G> SetType(String type){
         if(type.toUpperCase().equals("HashMap")){
             return new HashMap<>();
         }
@@ -36,6 +32,7 @@ public class Factory<K,V>{
 }
 
 
+//Código para hacer parte del Factory
 //https://www.geeksforgeeks.org/differences-treemap-hashmap-linkedhashmap-java/
 
 
