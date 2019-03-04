650cde094fc8e00d2e283bb161795f5f45d7d2c0
José Ovando <42974480+Jos260400@users.noreply.github.com>
2019-03-03 18:21:36 -0600

    Add files via upload

---
+++ Factory.java
@@ -0,0 +1,7 @@
+// Universidad del Valle de Guatemala
+// Curso: Algoritmos y Estructuras de Datos
+// Nombre: Fernando José Garavito Ovando	 Carné: 18071
+// Hoja de Trabajo No. 6
+public class Factory {
+
+}
